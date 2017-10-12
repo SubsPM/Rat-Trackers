a4df597035def80b5a5c686e5738b6eaa51d9173
Fredrik <forsberg.fredrik@hotmail.com>
2017-10-11 21:16:28 -0400

    Not a wokring version of M6

--- app/src/main/java/com/example/fredrik/rattrackers/LoggedInScreen.java
+++ app/src/main/java/com/example/fredrik/rattrackers/LoggedInScreen.java
@@ -1,21 +1,42 @@
 package com.example.fredrik.rattrackers;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 import android.view.View;
 
+import java.io.InputStream;
+import java.util.List;
+
 
 public class LoggedInScreen extends AppCompatActivity {
-
     private Button logOutButton;
+    private SQLiteDatabase mydatabase;
+    private Cursor todoCursor;
+    private ListView lvItems;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in_screen);
         logOutButton = (Button) findViewById(R.id.logOutButton);
+
+        //new DownloadFilesTask().execute();
+        // Attach cursor adapter to the ListView
+
+        mydatabase = openOrCreateDatabase("ratdata.db",MODE_PRIVATE,null);
+        todoCursor = mydatabase.rawQuery("SELECT  * FROM 'RatData.db';", null);
+        TodoCursorAdapter todoAdapter = new TodoCursorAdapter(this, todoCursor);
+
+        lvItems = (ListView) findViewById(R.id.listView);
+        lvItems.setAdapter(todoAdapter);
+
 
         logOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,4 +60,20 @@ public class LoggedInScreen extends AppCompatActivity {
             }
         });
     }
+
+    private class DownloadFilesTask extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            mydatabase = openOrCreateDatabase("ratdata.db",MODE_PRIVATE,null);
+            todoCursor = mydatabase.rawQuery("SELECT  * FROM ratdata.db", null);
+
+
+
+            return null;
+        }
+    }
+
 }
